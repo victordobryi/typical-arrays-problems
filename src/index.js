e470d35ec53c015c0507537469afdf142bb9e831
victOr <dobr1nya@mail.ru>
2020-10-01 10:47:18 +0300

    init: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array == undefined || array.length === 0 || array === null) {
+        return 0;
+    }
+    return Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == undefined || array.length === 0 || array === null) {
+        return 0;
+    }
+    return Math.max.apply(null, array);
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == undefined || array.length === 0 || array === null) {
+        return 0;
+    }
+    let total = array.reduce(function(a, b) {
+        return a + b;
+    });
+    let average = total / array.length;
+    return average;
+};
